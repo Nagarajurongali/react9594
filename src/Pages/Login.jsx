a207f36cb6d8249c7aa269216034ce3ea676f87b
Nagarajurongali <nagarajurongali369@gmail.com>
2025-02-13 10:22:04 +0530

    deploy

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 function Login() {
-    const [username , setUsername] = useState('');
-    const [password , setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    const submitHandler = (e)=>{
-        e.preventDefault();
-        console.log(username)
-        console.log(password)
-    }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(username)
+    console.log(password)
+  }
 
   return (
-    <div>
-        <form onSubmit={submitHandler}>
-            <input type="text" value={username} onChange={(e)=>{setUsername(e.target.value)}} placeholder='Enter Username' />
-            <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='Enter password' />
-            <input type="submit"  />
-        </form>
+    <div className='border p-3 m-auto w-50 '>
+      <p className='text-center text-primary'>Login FOrm</p>
+      <form onSubmit={submitHandler}>
+        <label>Enter Your Username</label>
+        <input type="text" value={username} className='form-control mb-2' onChange={(e) => { setUsername(e.target.value) }} />
+        <label>Enter Your password</label>
+        <input type="password" value={password} className='form-control mb-2' onChange={(e) => { setPassword(e.target.value) }} />
+        <label>Don't have an account?<Link to='/signup'>signup</Link> </label>
+        <Link to='/dashboard'><input type="submit" className='btn btn-primary w-100' /></Link>
+      </form>
     </div>
   )
 }
